3fbad2449e5baa5706341c1cc9993d5bca67d195
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-31 12:05:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
+
 function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 
 function increaseRankBy(n){
-  
+  const rank = document.querySelectorAll('ul.ranked-list li');
+  for(let i = 0; i < rank.length ; i++){
+    rank[i].innerHtml = (parseInt(rank[i].innerHtml) + n).toString;
+  }
 }
+
+
