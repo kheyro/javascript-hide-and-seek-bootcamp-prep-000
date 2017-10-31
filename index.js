f74672c408a4091f73afd37795c7353815c295ec
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-31 12:05:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget() {
 function increaseRankBy(n){
   const rank = document.querySelectorAll('ul.ranked-list li');
   for(let i = 0; i < rank.length ; i++){
-    rank[i].innerHtml = (parseInt(rank[i].innerHtml) + n).toString;
+    rank[i].innerHtml = (parseInt(rank[i].innerHtml) + n).toString();
   }
 }
 
